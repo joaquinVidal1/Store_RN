26a630c2076d8c12b4c9262c94d42e5710631339
Joaquin Vidal <joaquin.vidal@decemberlabs.com>
2023-12-04 15:11:51 -0300

    basic state without animations

--- src/features/cart/components/AnimatedCartItem.tsx
+++ src/features/cart/components/AnimatedCartItem.tsx
@@ -1,12 +1,5 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {StyleSheet} from 'react-native';
-import Animated, {
-  BounceInUp,
-  BounceOutDown,
-  useAnimatedStyle,
-  useSharedValue,
-  withTiming,
-} from 'react-native-reanimated';
 import {Product} from '../../products/types/Product';
 import {MARGIN_BETWEEN_COLUMNS} from '../CartScreen';
 import CartItem from './CartItem';
@@ -17,6 +10,7 @@ export type Props = {
   showMargin: boolean;
   wasItemDeleted: boolean;
   wasItemMoved: boolean;
+  onAnimationFinished: () => void;
 };
 
 const AnimatedCartItem: React.FC<Props> = ({
@@ -25,41 +19,42 @@ const AnimatedCartItem: React.FC<Props> = ({
   product,
   wasItemDeleted,
   wasItemMoved,
+  onAnimationFinished,
 }) => {
-  const translateY = useSharedValue(0);
-  const animatedStyle = useAnimatedStyle(() => {
-    return {
-      transform: [{translateY: translateY.value}],
-    };
-  }, []);
+  //   useEffect(() => {
+  //     if (wasItemDeleted) {
+  //       translateY.value = withTiming(-150, {duration: 500}, () => {});
+  //     }
+  //   }, [translateY, wasItemDeleted]);
 
-  useEffect(() => {
-    if (wasItemDeleted) {
-      translateY.value = withTiming(-150, {duration: 500}, () => {});
-    }
-  }, [translateY, wasItemDeleted]);
+  //   useEffect(() => {
+  //     if (wasItemMoved) {
+  //       translateY.value = withTiming(-150, {duration: 500});
+  //     }
+  //   }, [wasItemMoved, translateY]);
 
-  useEffect(() => {
-    if (wasItemMoved) {
-      translateY.value = withTiming(-150, {duration: 500});
-    }
-  }, [wasItemMoved, translateY]);
+  console.log('product name: ', product.name);
+  console.log('moved: ', wasItemMoved);
+  console.log('deleted: ', wasItemDeleted);
+  console.log('-------');
 
   return (
-    <Animated.View
-      entering={wasItemMoved ? undefined : BounceInUp}
-      exiting={wasItemDeleted ? BounceOutDown : undefined}
-      style={[animatedStyle]}>
-      <CartItem
-        product={product}
-        style={
-          showMargin
-            ? {marginEnd: MARGIN_BETWEEN_COLUMNS, ...styles.productStyle}
-            : styles.productStyle
-        }
-        onPress={onPress}
-      />
-    </Animated.View>
+    // <Animated.View
+    //   key={product.id}
+    //   //   entering={BounceInUp.duration(1000)}
+    //   layout={Layout.springify().delay(100)}
+    //   //   exiting={BounceOutDown.duration(1000)}>
+    // >
+    <CartItem
+      product={product}
+      style={
+        showMargin
+          ? {marginEnd: MARGIN_BETWEEN_COLUMNS, ...styles.productStyle}
+          : styles.productStyle
+      }
+      onPress={onPress}
+    />
+    // </Animated.View>
   );
 };
 
--- src/features/cart/components/CartList.tsx
+++ src/features/cart/components/CartList.tsx
@@ -1,13 +1,12 @@
-import React, {useCallback, useEffect, useMemo, useState} from 'react';
+import React, {useCallback, useMemo, useState} from 'react';
 import {
-  Animated,
+  // Animated,
   Dimensions,
-  FlatList,
   StyleSheet,
   useWindowDimensions,
   View,
 } from 'react-native';
-import {StyleProps} from 'react-native-reanimated';
+import Animated, {LinearTransition, StyleProps} from 'react-native-reanimated';
 import {useAppSelector} from '../../../infrastructure/store/hooks/hooks';
 import {useProducts} from '../../products/queries';
 import {Product} from '../../products/types/Product';
@@ -21,6 +20,12 @@ import AnimatedCartItem from './AnimatedCartItem';
 const RowsSeparator = () => {
   return <View style={{height: 20}} />;
 };
+
+function arraysEqual(a, b) {
+  a = Array.isArray(a) ? a : [];
+  b = Array.isArray(b) ? b : [];
+  return a.length === b.length && a.every((el, ix) => el === b[ix]);
+}
 
 const CartList = ({
   style,
@@ -36,56 +41,101 @@ const CartList = ({
   const {data: apiProducts} = useProducts();
   const cart = useAppSelector(state => state.cart.cart);
   const {width} = useWindowDimensions();
-
-  const explote = useCallback(async () => {
-    const deletedItems = prevList.filter(
-      prevItem => !cart.some(currentItem => currentItem.id === prevItem.id),
-    );
-
-    const promises = deletedItems.map(async product => {
-      const itemIndex = displayList.findIndex(item => item.id === product.id);
-      console.log('cart', cart);
-      console.log('itemIndex: ', itemIndex);
-      console.log('displayList: ', displayList);
-      await onExplode(itemIndex);
-    });
-
-    if (promises && promises.length > 0) {
-      Promise.all(promises).then(() => setShowPrev(false));
-      setPrevList(displayList);
-    } else {
-      setShowPrev(false);
-      setPrevList(displayList);
-    }
-  }, [cart]);
-
-  useEffect(() => {
-    explote();
-  }, [explote]);
-
   const [prevList, setPrevList] = useState<Product[]>([]);
 
+  // const explote = useCallback(async () => {
+  //   const deletedItems = prevList.filter(
+  //     prevItem => !cart.some(currentItem => currentItem.id === prevItem.id),
+  //   );
+
+  //   const promises = deletedItems.map(async product => {
+  //     const itemIndex = displayList.findIndex(item => item.id === product.id);
+  //     console.log('cart', cart);
+  //     console.log('itemIndex: ', itemIndex);
+  //     console.log('displayList: ', displayList);
+  //     await onExplode(itemIndex);
+  //   });
+
+  //   if (promises && promises.length > 0) {
+  //     Promise.all(promises).then(() => setShowPrev(false));
+  //     setPrevList(displayList);
+  //   } else {
+  //     setShowPrev(false);
+  //     setPrevList(displayList);
+  //   }
+  // }, [cart]);
+
+  // useEffect(() => {
+  //   explote();
+  // }, [explote]);
+
+  const updatePrevList = useCallback(
+    async (displayList: Product[]) => {
+      const sleep = (ms: number) => {
+        return new Promise(resolve => {
+          setTimeout(resolve, ms);
+        });
+      };
+
+      await sleep(1000);
+
+      setPrevList(displayList);
+      setShowPrev(false);
+    },
+    [setPrevList, setShowPrev],
+  );
+
+  const displayList2: Product[] = useMemo(() => {
+    return cart
+      .map(cartItem => {
+        const product = apiProducts?.find(
+          apiProduct => apiProduct.id === cartItem.id,
+        );
+        return product ? {...product, quantity: cartItem.quantity} : undefined;
+      })
+      .filter(product => product) as Product[];
+  }, [apiProducts, cart]);
+
   const displayList: Product[] = useMemo(() => {
-    return showPrev
-      ? prevList
-        ? prevList
-        : []
-      : (cart
-          .map(cartItem => {
-            const product = apiProducts?.find(
-              apiProduct => apiProduct.id === cartItem.id,
-            );
-            return product
-              ? {...product, quantity: cartItem.quantity}
-              : undefined;
-          })
-          .filter(product => product) as Product[]);
+    if (showPrev) {
+      // if (
+      //   JSON.stringify(prevList) !== JSON.stringify(displayList) &&
+      //   displayList
+      // ) {
+      //   console.log('prevList: ', JSON.stringify(prevList));
+      //   console.log('displayList: ', JSON.stringify(displayList));
+      //   updatePrevList(displayList);
+      // }
+      return prevList ? prevList : [];
+    } else {
+      return cart
+        .map(cartItem => {
+          const product = apiProducts?.find(
+            apiProduct => apiProduct.id === cartItem.id,
+          );
+          return product
+            ? {...product, quantity: cartItem.quantity}
+            : undefined;
+        })
+        .filter(product => product) as Product[];
+    }
   }, [apiProducts, cart, prevList, showPrev]);
 
-  const scaleValues = useMemo(
-    () => displayList.map(() => new Animated.Value(1)),
-    [displayList],
-  );
+  // const displayList: Product[] = useMemo(() => {
+  //   return cart
+  //     .map(cartItem => {
+  //       const product = apiProducts?.find(
+  //         apiProduct => apiProduct.id === cartItem.id,
+  //       );
+  //       return product ? {...product, quantity: cartItem.quantity} : undefined;
+  //     })
+  //     .filter(product => product) as Product[];
+  // }, [apiProducts, cart]);
+
+  // const scaleValues = useMemo(
+  //   () => displayList.map(() => new Animated.Value(1)),
+  //   [displayList],
+  // );
 
   const onExplode = async (index: number) => {
     console.log('onExplode for index: ', index);
@@ -112,9 +162,21 @@ const CartList = ({
     (width - 2 * MARGIN_HORIZONTAL - MARGIN_BETWEEN_COLUMNS) / IMAGE_SIZE,
   );
   const maxWidth = Dimensions.get('window').width - 3 * MARGIN_HORIZONTAL;
+
+  const renderCell = useCallback(props => {
+    return (
+      <Animated.View
+        {...props}
+        layout={LinearTransition.duration(3000)}
+        // entering={BounceInUp.duration(1000)}
+        // exiting={BounceOutDown.duration(1000)}
+      />
+    );
+  }, []);
+
   return (
-    <FlatList
-      data={displayList}
+    <Animated.FlatList
+      data={displayList2}
       contentContainerStyle={{
         paddingTop: 24,
         marginHorizontal: MARGIN_HORIZONTAL,
@@ -124,18 +186,39 @@ const CartList = ({
         <AnimatedCartItem
           product={item}
           onPress={() => {
-            setPrevList(displayList);
+            // setPrevList(displayList);
             onProductPressed(item);
           }}
           showMargin={index % 2 === 0}
-          wasItemDeleted={false}
-          wasItemMoved={false}
+          wasItemDeleted={
+            prevList
+              ? prevList
+                  .filter(
+                    prevItem =>
+                      !cart.some(currentItem => currentItem.id === prevItem.id),
+                  )
+                  .find(prevItem => prevItem.id === item.id) !== undefined
+              : false
+          }
+          wasItemMoved={
+            prevList
+              ? prevList.findIndex(product => product.id === item.id) !==
+                  cart.findIndex(product => product.id === item.id) &&
+                cart.findIndex(product => product.id === item.id) !== -1
+              : false
+          }
+          onAnimationFinished={() => {
+            setShowPrev(false);
+            setPrevList(displayList);
+          }}
         />
       )}
       keyExtractor={product => product.id.toString()}
       numColumns={numColumns}
       style={[styles.list, style]}
       ItemSeparatorComponent={RowsSeparator}
+      CellRendererComponent={renderCell}
+      // layout={LinearTransition.duration(3000)}
     />
   );
 };
